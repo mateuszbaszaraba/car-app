793fe6b0fa13ee0c2b5fb285d53e771984944183
Matt <mateuszbaszaraba@outlook.com>
2020-04-27 23:17:35 +0200

    updated store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,5 +5,21 @@
             iure, dolorem, esse nemo maiores praesentium ipsum laborum libero fugit 
             asperiores architecto. Dolorum nulla cumque magnam sequi numquam repellat 
             asperiores!</p>
+
+            <ul>
+                <li v-for="car in carsy" :key="car.id">
+                    <div v-for="(value, key) in car" :key="value.id">{{ key }}: {{ value }}</div>
+                </li>
+            </ul>
     </div>
 </template>
+
+<script>
+export default {
+    computed: {
+        carsy() {
+            return this.$store.state.cars
+        }
+    }
+}
+</script>
--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,10 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        
+        cars: [
+            { brand: 'bmw', model: 'serie 3', mileage: '142232' },
+            { brand: 'mercedes-benz', model: 'a-class', mileage: '111814' },
+            { brand: 'bmw', model: 'serie 5', mileage: '123571' },
+        ]
     }
 })
