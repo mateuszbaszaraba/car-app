8b05281768d4b760496f90f13e205cb91fd973f3
Matt <mateuszbaszaraba@outlook.com>
2020-04-30 13:50:40 +0200

    test

--- src/components/Car/Car.vue
+++ src/components/Car/Car.vue
@@ -2,6 +2,7 @@
     <div class="car-page">
         <h1>Car Page</h1>
         <p>Developing version</p>
+        <p>test test commit</p>
         <router-view></router-view>
     </div>
 </template>
